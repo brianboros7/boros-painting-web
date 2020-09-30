a80b90bb9a0fa016ed5341efd92596631ededad3
Brian Boros <Bpower@tests-MBP.attlocal.net>
2020-09-29 23:08:27 -0500

    all images on Gallery page are working, however, flex is reducing each added image size accordingly, with no break.

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -4,25 +4,31 @@ import interiorImages from './interiorImages';
 import exteriorImages from'./exteriorImages'; 
 
 function Gallery() {
+   {/* const [galleryImage, setGalleryImage] = useEffect("") */}
+    
+    const interiorGalleryImages = interiorImages.map((image) => (
+        <div key={image.id}>
+            <span className={image.span_id}> 
+              <img src={image.src} alt={image.alt} />
+            </span> 
+        </div> 
+    ))
 
-    const interiorGalleryImages = () => {
-        {interiorImages.map((image) => ( 
-            <img key={image.id} src={image.src} alt={image.alt} /> 
-        ))} 
-    }
-    const exteriorGalleryImages = () => {
-        {exteriorImages.map((image) => ( 
-            <img key={image.id} src={image.src} alt={image.alt} /> 
-        ))} 
-    }
+    const exteriorGalleryImages = exteriorImages.map((image) => (
+        <div key={image.id}>
+            <span className={image.span_id}> 
+              <img src={image.src} alt={image.alt} />
+            </span> 
+        </div> 
+    ))
 
     return(
         <main className="gallery-page">
             <section className="gallery-section">
                 <h2>Interior Gallery</h2>
                 <div className="gallery-images-container border">
-                    <div className="gallery-images">
-                        <div className="gallery-image"> 
+                    <div className="gallery-images-wrapper">
+                        <div className="gallery-images"> 
                             {interiorGalleryImages}
                         </div> 
                     </div>
@@ -31,8 +37,8 @@ function Gallery() {
             <section className="gallery-section">
                 <h2>Exterior Gallery</h2> 
                 <div className="gallery-images-container border">
-                    <div className="gallery-images">
-                        <div className="gallery-image"> 
+                    <div className="gallery-images-wrapper">
+                        <div className="gallery-images"> 
                             {exteriorGalleryImages}
                         </div>
                     </div>
--- src/pages/gallery/interiorImages.js
+++ src/pages/gallery/interiorImages.js
@@ -2,30 +2,46 @@ import interiorImage1 from './../../assets/images/interior/boros-painting-interi
 import interiorImage2 from './../../assets/images/interior/boros-painting-interior-before-and-after-2.png'; 
 import interiorImage3 from './../../assets/images/interior/boros-painting-interior-before-and-after-3.png'; 
 import interiorImage4 from './../../assets/images/interior/boros-painting-interior-before-and-after-4.png'; 
-
-
+import interiorImage5 from './../../assets/images/interior/boros-painting-interior-before-and-after-5.png'; 
+import interiorImage6 from './../../assets/images/interior/boros-painting-interior-before-and-after-6.png'; 
 
 
 const interiorImages = [
     {
         id: "1",
         src: interiorImage1,
-        alt: "Interior before and after image One"
+        alt: "Interior before and after image One",
+        span_id: "span-1", 
     },
     {
         id: "2", 
         src: interiorImage2,
-        alt: "Interior before and after image two"
+        alt: "Interior before and after image two",
+        span_id: "span-2",
     },
     {
         id: "3",
         src: interiorImage3,
-        alt: "Interior before and after three"
+        alt: "Interior before and after three",
+        span_id: "span-3", 
     },
     {
         id: "4",
         src: interiorImage4,
-        alt: "Interior before and after image four"
+        alt: "Interior before and after image four",
+        span_id: "span-4", 
+    },
+    {
+        id: "5",
+        src: interiorImage5,
+        alt: "Exterior before and after image five",
+        span_id: "span-5", 
+    },
+    {
+        id: "6",
+        src: interiorImage6,
+        alt: "Interior before and after image six",
+        span_id: "span-5"
     }
 ]
 
