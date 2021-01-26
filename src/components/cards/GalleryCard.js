09460e0696311f0798857611bf225c6129b9641c
Brian Boros <Bpower@tests-MBP.attlocal.net>
2021-01-26 10:55:12 -0600

    added gallery card files

---
+++ src/components/cards/GalleryCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+fumction GalleryCard() {
+
+    return(
+        <div> 
+            <Card>
+                <Card.Img> </Card.Img>
+            </Card>
+        </div>
+    )
+}
